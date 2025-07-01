3eb84915e1222980da0dc9ea4788f17377fe12b3
camille.sun <xrs.sun@gmail.com>
2025-07-01 11:05:12 +0100

    updates InfoPageError

--- src/test/scala/locators/CheckoutLocators.scala
+++ src/test/scala/locators/CheckoutLocators.scala
@@ -7,7 +7,7 @@ object CheckoutLocators {
   val LastName: By = By.id("last-name")
   val PostalCode:By = By.id("postal-code")
   val Continue: By= By.id("continue")
-  val Error: By = By.className("error-button")
+  val InfoPageError: By = By.xpath("//*[@id='checkout_info_container']/div/form/div[1]/div[4]/h3")
   val Finish:By= By.id("finish")
   val ItemTotal:By= By.className("summary_total_label")
   val checkoutCompleted = "checkout-complete.html"
